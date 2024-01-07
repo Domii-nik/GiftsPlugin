f9418410a52e5bae99284378c6049c266d731a91
Domii <117467569+Domii-nik@users.noreply.github.com>
2024-01-07 18:38:21 +0100

    Joa, erste Version halt

--- src/main/java/de/domii/gifts/listener/GiftClaimListener.java
+++ src/main/java/de/domii/gifts/listener/GiftClaimListener.java
@@ -14,23 +14,31 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class GiftClaimListener implements Listener {
     private Gifts main;
-    public GiftClaimListener(Gifts main) { this.main = main; }
+
+    public GiftClaimListener(Gifts main) {
+        this.main = main;
+    }
+
     @EventHandler
     public void onGiftClaim(PlayerInteractEvent e) {
         Block block = e.getClickedBlock();
 
-        if (block.getType() != Material.PLAYER_HEAD) {
+        if (block == null || block.getType() != Material.PLAYER_HEAD) {
             return;
         }
+
         Location location = block.getLocation();
         Player player = e.getPlayer();
 
         if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
             FileConfiguration gifts = YamlConfiguration.loadConfiguration(new File(main.getDataFolder(), "gifts.yml"));
             ConfigurationSection giftLocs = gifts.getConfigurationSection("Gift-Locations");
+
             for (String giftID : giftLocs.getKeys(false)) {
                 ConfigurationSection giftData = giftLocs.getConfigurationSection(giftID);
                 double x = giftData.getDouble("x");
@@ -38,15 +46,28 @@ public class GiftClaimListener implements Listener {
                 double z = giftData.getDouble("z");
 
                 if (location.getX() == x && location.getY() == y && location.getZ() == z) {
-                    FileConfiguration playerData = YamlConfiguration.loadConfiguration(new File(main.getDataFolder(), "playerData.yml"));
+                    File playerDataFile = new File(main.getDataFolder(), "playerData.yml");
+                    FileConfiguration playerData = YamlConfiguration.loadConfiguration(playerDataFile);
+
                     ConfigurationSection playersSection = playerData.getConfigurationSection("Player-Data");
                     ConfigurationSection playerSection = playersSection.getConfigurationSection(player.getUniqueId().toString());
+
                     if (playerSection == null) {
-                        playerData.createSection(player.getUniqueId().toString());
-                        playerSection = playerData.getConfigurationSection(player.getUniqueId().toString());
+                        playerData.createSection("Player-Data." + player.getUniqueId().toString());
+                        playerSection = playerData.getConfigurationSection("Player-Data." + player.getUniqueId().toString());
                         playerSection.set("Name", player.getName());
+                        playerSection.set("claimed-Gifts", playerSection.getStringList("claimed-Gifts"));
                     }
-                    playerSection.getStringList("claimed-Gifts").add(giftID);
+
+                    List<String> claimedGifts = playerSection.getStringList("claimed-Gifts");
+                    claimedGifts.add(giftID);
+                    playerSection.set("claimed-Gifts", claimedGifts);
+
+                    try {
+                        playerData.save(playerDataFile);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
 
                     if (main.getConfig().getBoolean("delete-Gift-after-collect")) {
                         // DELETE
